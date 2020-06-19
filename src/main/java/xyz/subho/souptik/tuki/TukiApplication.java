31b2a10ae215365810eb91537dc76f4739471567
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-06-19 21:34:48 +0530

    Initial commit

---
+++ src/main/java/xyz/subho/souptik/tuki/TukiApplication.java
@@ -0,0 +1,13 @@
+package xyz.subho.souptik.tuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TukiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/subho/souptik/tuki/TukiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.subho.souptik.tuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
